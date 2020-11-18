36510c83fb1c500ec318984fd67d9cded5a11ba7
bobbyseptianto <bobbyseptianto@gmail.com>
2020-11-18 23:28:18 +0700

    Preparing Deploy Client Customer

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://ikeha-ecommerce-cms-bob.herokuapp.com'
 })
 
 export default instance
