1613dfab575d6d5b0a885d74fc3c0fa530a58895
Deo Mareza <deomareza@gmail.com>
2020-11-19 11:33:52 +0700

    changed axios address to deployed heroku server

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'https://deo-ecommerce-cms.herokuapp.com'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://deo-ecommerce-cms.herokuapp.com'
+  // baseURL: 'http://localhost:3000'
 })
 
 export default instance
