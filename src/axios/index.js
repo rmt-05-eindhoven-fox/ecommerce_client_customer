d312618a94a8bf38debc9cf71e285a38e59cb16f
AgungSetyaPratama <agungwas@gmail.com>
2020-11-20 12:43:43 +0700

    change server

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
-export default axios.create({ baseURL: 'http://localhost:3001' })
-// export default axios.create({ baseURL: 'https://e-commerce-server-pratama.herokuapp.com/' })
+// export default axios.create({ baseURL: 'http://localhost:3001' })
+export default axios.create({ baseURL: 'https://e-commerce-server-pratama.herokuapp.com/' })
